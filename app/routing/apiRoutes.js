48297036fcda3d19d26269b38645ff2095160bd8
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-05-04 22:37:03 -0400

    commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,3 @@
-// Your apiRoutes.js file should contain two routes:
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. 
-// This route will also be used to handle the compatibility logic.
-
 var friends = require("./../data/friend.js")
   
   module.exports = function(app) {
@@ -33,15 +28,8 @@ var friends = require("./../data/friend.js")
 
       friends.push(newFriend)
 
-      //loop through user scores
-      //loop through friends scores
-      //friends - user scores
-
 
        res.json(friendMatch);
-        // var newfriends = req.body;
-        // newfriends.toString();
-        // friends.push(newfriends);
-        // res.json(newfriends);
+   
       });
   };
